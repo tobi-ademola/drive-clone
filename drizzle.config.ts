24e802fe756a9137d76cdaa5cce58f9089dd741c
Tobi Ademola <meademolatobi@gmail.com>
2025-04-14 12:45:27 +0100

    Database added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,29 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env";
 
-export default {
+// export default defineConfig({
+//   schema: "./src/server/db/schema.ts",
+//   dialect: "singlestore",
+//   dbCredentials: {
+//     host: env.SINGLESTORE_HOST,
+//     user: env.SINGLESTORE_USER,
+//     password: env.SINGLESTORE_PASS,
+//     port: env.SINGLESTORE_PORT,
+//     database: env.SINGLESTORE_DB_NAME,
+//     ssl: {},
+//   },
+//   tablesFilter: ["drive-clone_*"],
+// });
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    port: parseInt(env.SINGLESTORE_PORT),
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
   },
-  tablesFilter: ["drive-clone_*"],
-} satisfies Config;
+});
