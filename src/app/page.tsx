a7117773dd0ed48f6824bb0d5028e416eb8f8791
Tobi Ademola <meademolatobi@gmail.com>
2025-04-21 13:46:25 +0100

    folder ids are now handled via the routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import {
-  files as filesSchema,
-  folders as foldersSchema,
-} from "~/server/db/schema";
-import { db } from "~/server/db";
-import DriveContents from "./drive-contents";
-
-export default async function GoogleDriveClone() {
-  const files = await db.select().from(filesSchema);
-  const folders = await db.select().from(foldersSchema);
-
-  return <DriveContents files={files} folders={folders} />;
+export default async function HomePage() {
+  return <div>Google Drive Clone</div>;
 }
