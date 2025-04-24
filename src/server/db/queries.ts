75208e1873b45676c9c43e7ffd348c4a8ee32e33
Tobi Ademola <meademolatobi@gmail.com>
2025-04-24 21:03:43 +0100

    minor restructure to upload logic

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -48,12 +48,13 @@ export const MUTATIONS = {
       name: string;
       size: number;
       url: string;
+      parent: number;
     };
     userId: string;
   }) {
     return await db.insert(filesSchema).values({
       ...input.file,
-      parent: 1,
+      parent: input.file.parent,
     });
   },
 };
