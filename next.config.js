29fc58261ad3d41017691335b45106f1dba44949
Tobi Ademola <meademolatobi@gmail.com>
2025-04-29 09:49:31 +0100

    Adjusted rewrites configurations

--- next.config.js
+++ next.config.js
@@ -12,23 +12,6 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://eu-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://eu.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://eu.i.posthog.com/decide",
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
 };
 
 export default config;
